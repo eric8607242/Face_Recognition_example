69859179555238bf3fa02871526a7fffde06a09d
eric8607242 <e0928021388@gmail.com>
2021-02-17 22:31:20 +0800

    First commit, loss function, and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Face_Recognition_example
+
+This is a example repo for face recognition. We cover the basic component for face recognition. (e.g., loss function, data loader, and inference)
+
+**The main components in this repo.**
+* Loss function
+    * SphereFace
+    * CosFace
+    * ArgFace
+
+**Actually, I am still confused about some implementation detail for face recognition. Hope anyone can help me to clarify more implementation detail.**
---
+++ face_criterion.py
@@ -0,0 +1,140 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SphereFace(nn.Module):
+    """
+    SphereFace loss function.
+    Paper : https://arxiv.org/pdf/1704.08063.pdf
+    
+    The annealing optimization strategy for A-Softmax loss.
+    """
+    def __init__(self, embeddings_size, class_nums, margin, s):
+        super(SphereFace, self).__init__()
+
+        self.identity_weights = nn.Parameter(torch.Tensor(embeddings_size, class_nums))
+
+        self.margin = margin
+        self.s = s
+
+        # The hyperparameter of annealing optimization
+        self.MAXLAMBDA = None
+        self.MINLAMBDA = None
+        self.lamb = self.MAXLAMBDA
+        # ============================================
+
+        # Double angle formula.
+        self.angle_formula = [
+                    lambda x: x ** 0,                           # 0 * \theta
+                    lambda x: x ** 1,                           # 1 * \theta
+                    lambda x: 2 * x ** 2 - 1,                   # 2 * \theta
+                    lambda x: 4 * x ** 3 - 3 * x,               # 3 * \theta
+                    lambda x: 8 * x ** 4 - 8 * x ** 2 + 1,      # 4 * \theta
+                    lambda x: 16 * x ** 5 - 20 * x ** 3 + 5 * x # 5 * \theta
+                ]
+
+
+    def forward(self, embeddings, label):
+        identity_weights_norm = F.normalize(self.identity_weights, p=2)
+
+        cos_theta = torch.mm(embeddings, identity_weights_norm)
+        cos_theta = cos_theta.clamp(-1, 1)
+
+        cos_m_theta = self.angle_formula[self.margin](cos_theta)
+
+        # Calculate k for the phi which make m * \theta will not out of the range [0, pi]
+        # Why we can calculate k by these formulate?
+        theta = cos_theta.data.acos()
+        k = (self.margin * theta / math.pi).floor()
+        phi_theta = ((-1.0)**k) * cos_m_theta - 2*k
+
+        x_norm = torch.norm(embeddings, 2, 1)
+
+        one_hot = torch.zeros(cos_theta.size(), device=cos_theta.device)
+        one_hot.scatter_(dim=1, index=label.view(-1, 1), src=1) # set the value to 1 according to the label
+
+        # Annealing optimization. Annealing from Softmax to A-Softmax, which make training more stable.
+        output = (one_hot * (phi_theta - cos_theta)) / (1 + self.lamb)) + cos_theta # Utilize annealing for gt class. Otherwise utilize normalize cos_theta
+        output *= x_norm.view(-1, 1)
+
+        return output
+
+
+class CosFace(nn.Module):
+    """
+    Cosface loss function.
+    Paper : https://arxiv.org/pdf/1801.09414.pdf
+    """
+    def __init__(self, embeddings_size, class_nums, margin, s):
+        super(CosFace, self).__init__()
+
+        self.identity_weights = nn.Parameter(torch.Tensor(embeddings_size, class_nums))
+
+        self.margin = margin
+        self.s = s
+
+    def forward(self, embeddings):
+        identity_weights_norm = F.normalize(self.identity_weights, p=2)
+
+        cos_theta = torch.mm(embeddings, identity_weights_norm)
+        cos_theta = cos_theta.clamp(-1, 1)
+
+        cos_theta_m = cos_theta - self.margin # cos(\theta) - m
+
+        one_hot = torch.zeros(cos_theta.size(), device=cos_theta.device)
+        one_hot.scatter_(dim=1, index=label.view(-1, 1), src=1) # set the value to 1 according to the label
+
+        output = (one_hot * cos_theta_m) + ((1.0 - one_hot)*cos_theta) # Add margin to the gt class. Otherwise utilize normalize cos_theta
+        output = output * self.s
+
+        return output
+
+
+class ArcFace(nn.Module):
+    """
+    Arcface loss function.
+    Paper : https://arxiv.org/pdf/1801.07698.pdf
+    """
+    def __init__(self, embeddings_size, class_nums, margin, s):
+        super(ArcFace, self).__init__()
+
+        self.identity_weights = nn.Parameter(torch.Tensor(embeddings_size, class_nums))
+
+        self.margin = margin
+        self.threshold = math.cos(math.pi - margin)
+        self.margin_cosface = math.sin(math.pi - margin) * margin
+
+        self.s = s
+
+    def forward(self, embeddings):
+        identity_weights_norm = F.normalize(self.identity_weights, p=2)
+
+        cos_theta = torch.mm(embeddings, identity_weights_norm)
+        cos_theta = cos_theta.clamp(-1, 1)
+
+        cos_theta_2 = torch.pow(cos_theta, 2) # cos(\theta)^2
+        sin_theta_2 = 1 - cos_theta_2 # sin(\theta)^2
+
+        sin_theta = torch.sqrt(sin_theta_2)
+
+        # cos(a - b) = cos(a)cos(b) - sin(a)sin(b)
+        cos_theta_m = cos_theta * cos_m - sin_theta * sin_m # cos(\theta - m)
+
+
+        # Control \theta + m range in [0, pi] ============================
+        # when \theta + m >= pi, use cosface instead. Why?
+        cos_theta_m = torch.where(cos_theta > self.threshold, cos_theta_m, cos_theta - self.mm)
+        # ================================================================
+
+        one_hot = torch.zeros(cos_theta.size(), device=cos_theta.device)
+        one_hot.scatter_(dim=1, index=label.view(-1, 1), src=1) # set the value to 1 according to the label
+
+        output = (one_hot * cos_theta_m) + ((1.0 - one_hot)*cos_theta) # Add margin to the gt class. Otherwise utilize normalize cos_theta
+        output = output * self.s
+
+        return output
+
+        
