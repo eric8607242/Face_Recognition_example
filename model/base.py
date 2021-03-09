8c5976b576e2047aae17fff29265a172054d71f0
eric8607242 <e0928021388@gmail.com>
2021-03-10 00:26:48 +0800

    modify network utils, move IRBlock into nobilefacenet model

--- model/base.py
+++ model/base.py
@@ -5,31 +5,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .network_utils import get_block
-
 class BaseModel(nn.Module):
     def __init__(self):
         super(BaseModel, self).__init__()
-
-    def _construct_model(self, model_config, bn_momentum=0.1, bn_track_running_stats=True):
-        layers = nn.ModuleList()
-        for l_cfg in model_config:
-            block_type, in_channels, out_channels, stride, kernel_size, group, activation, se, kwargs = l_cfg
-
-            layer = get_block(block_type=block_type,
-                              in_channels=in_channels,
-                              out_channels=out_channels,
-                              kernel_size=kernel_size,
-                              stride=stride,
-                              activation=activation,
-                              se=se,
-                              group=group,
-                              bn_momentum=bn_momentum,
-                              bn_track_running_stats=bn_track_running_stats,
-                              **kwargs)
-
-            layers.append(layer)
-        return layers
 
     def _initialize_weights(self):
         for m in self.modules():
